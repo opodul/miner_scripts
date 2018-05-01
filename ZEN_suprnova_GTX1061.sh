9ecd72c1b4332a6dd322804f13ff0a5df7e747f4
opodul <neyludfr@gmail.com>
2018-05-01 10:23:44 +0200

    add ZenCash script for SuprNova

---
+++ ZEN_suprnova_GTX1061.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+. ./config.sh
+cuda_dev=$cuda_1061
+worker_name=EVGA61
+
+echo -ne "\033]0;"ZEN suprnova CUDA $cuda_dev"\007"
+
+cd $path_ccminer
+#cd $path_suprminer
+
+if ![[ $cuda_dev =~ ^[0-9]+$ ]] ; then
+      echo "device_cuda is NA or is not a number: $cuda_dev"
+   read toto
+   exit 1
+fi
+
+./ccminer -a equihash -o stratum+tcp://zen.suprnova.cc:3620 -u $suprnova_weblogin.$worker_name -p $suprnova_pass -d $cuda_dev
+
