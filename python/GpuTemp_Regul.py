495715f3cbe271a12867049d1b8a95fc82a0c2e7
opodul <neyludfr@gmail.com>
2019-01-13 20:26:58 +0100

    read/write in 1 file for all Gpu, request temperature/freq only once for all gpu

--- python/GpuTemp_Regul.py
+++ python/GpuTemp_Regul.py
@@ -3,7 +3,7 @@ import os
 import re
 import time
 import math
-#import threading, subprocess, signal
+import json
 
 #--------------------------
 # module variable
@@ -14,91 +14,106 @@ TEMP_KP_OFFSET=2.0
 KI_UP=0.2
 KI_DOWN=KI_UP/5.0
 TEMP_TARGET=71.0
-TEMP_DIFF_MIN=-15.0
+TEMP_TARGET_MIN=TEMP_TARGET-15.0
 
 GPUFREQ_MIN=500
 
+g_NbGpu=0;
+g_RegulInfo_Ipart=[]
+g_RegulInfo_Fixpart=[]
+g_RegulInfo_OkCnt=[]
+g_RegulInfo_OkPwm=[]
+g_RegulInfo_GpuTemp=[]
+g_RegulInfo_GpuFreq=[]
+
 #--------------------------
-def get_GpuTemp( index=0 ):
-    cmd="nvidia-settings -q [gpu:"+str(index)+"]/GPUCoreTemp | grep gpu:"+str(index)
-    o=os.popen(cmd).read()
-    m=re.search('([0-9]+)\.$', o)
-    if m is not None:
-        temp=float( m.group(1) )
-    else:
-        temp=99.0
-        
+def get_AllGpuTemp():
+    cmd="nvidia-settings -q [gpu]/GPUCoreTemp"
+    lines=os.popen(cmd).readlines()
+    
+    temp=[]
+    for line in lines:
+        m=re.search('Attribute.*gpu:([0-9]).* ([0-9]+)\.', line)
+        if m is not None:
+            #dbg: print(line)
+            #todo: gpu_idx=int(m.group(1)) # gpu index is not verified, we assume to get those in the correct order
+            gpu_temp= float(m.group(2))
+            temp.append(gpu_temp)
     return temp
     
 #--------------------------
-def get_GpuFreq( index=0 ):
-    cmd ="nvidia-settings -q [gpu:"+str(int(index))+"]/GPUCurrentClockFreqs"
-    cmd+=" |grep gpu:"+str(int(index))
-    o=os.popen(cmd).read()
-    m=re.search('([0-9]+),([0-9]+)\.$', o)
-    if m is not None:
-        freq1=float( m.group(1) )
-    else:
-        freq1=9999.0
-        
-    return freq1
+def get_AllGpuFreq():
+    cmd="nvidia-settings -q [gpu]/GPUCurrentClockFreqs"
+    lines=os.popen(cmd).readlines()
+    
+    freq=[]
+    for line in lines:
+        m=re.search('Attribute.*gpu:([0-9]).* ([0-9]+),([0-9]+)\.', line)
+        if m is not None:
+            #dbg: print(line)
+            #todo: gpu_idx=int(m.group(1)) # gpu index is not verified, we assume to get those in the correct order
+            gpu_freq= float(m.group(2))
+            #m.group(3) is the memory freq
+            freq.append(gpu_freq)
+    return freq
     
 #-------------------------- 
 def set_FanSpeedAuto( index=0 ): 
     cmd ="nvidia-settings -a [gpu:"+str(int(index))+"]/GPUFanControlState=0"
     os.popen(cmd)
     return
-  
-#-------------------------- 
-def set_FanSpeedManu( index=0 ): 
-    cmd ="nvidia-settings -a [gpu:"+str(int(index))+"]/GPUFanControlState=1"
-    os.popen(cmd)
-    return
     
 #-------------------------- 
-def set_FanSpeedValue( value=80 ,index=0 ):
-    cmd ="nvidia-settings -a [fan:"+str(int(index))+"]/GPUTargetFanSpeed="+str(int(value))
+def set_FanSpeed( index=0 , pwm=95 ):
+    cmd ="nvidia-settings"
+    cmd+=" -a [gpu:"+str(int(index))+"]/GPUFanControlState=1"
+    cmd+=" -a [fan:"+str(int(index))+"]/GPUTargetFanSpeed="+str(int(pwm))
     os.popen(cmd)
+    #print(cmd)
     return
 
 #-------------------------- 
-def write_FanSpeed( value , index=0,  ): 
-    with open("fan"+str(index)+".cfg","w") as f:
-        f.write(str(value))
+def write_AllFanSpeed( values ): 
+    with open("fanSpeed.json","w") as f:
+        json.dump(values,f)
     return
     
 #--------------------------  
-def read_FanSpeed( index=0 ): 
+def read_AllFanSpeed( ): 
     try:
-        with open("fan"+str(index)+".cfg","r") as f:
-            value=float(f.read())
+        with open("fanSpeed.json","r") as f:
+            values = json.load(f)
     except IOError:
-        value=50.0
-    return value
+        values=[50.0, 50.0, 50.0, 50.0]
+    return values
     
 #-------------------------- 
 def Regul_Loop(index=0):
-    temp_curr=get_GpuTemp(index)
+
+    global g_RegulInfo_GpuFreq  
+    global g_RegulInfo_OkCnt  
+    
+    temp_curr=g_RegulInfo_GpuTemp[index]
     temp_diff=temp_curr-TEMP_TARGET
     
     #set default value
-    pwm_val = Regul_Loop.Fixpart[index]   
+    pwm_val = g_RegulInfo_Fixpart[index]   
     
     #temperature low enough --> shut off the fan
-    if temp_diff < TEMP_DIFF_MIN:
+    if temp_curr < TEMP_TARGET_MIN:
         pwm_val = 5
    
     #no GPU activity --> shut off the fan
-    elif get_GpuFreq(index) < GPUFREQ_MIN:
+    elif g_RegulInfo_GpuFreq[index] < GPUFREQ_MIN and temp_curr < TEMP_TARGET:
         pwm_val = 4
         
     #temparature higher than target
     elif 0 < temp_diff:
-        Regul_Loop.Ipart[index]+=temp_diff*KI_UP
-        if 30.0 < Regul_Loop.Ipart[index]:
-            Regul_Loop.Ipart[index] = 30.0
+        g_RegulInfo_Ipart[index]+=temp_diff*KI_UP
+        if 50.0 < g_RegulInfo_Ipart[index]:
+            g_RegulInfo_Ipart[index] = 50.0
         
-        pwm_val+=Regul_Loop.Ipart[index] 
+        pwm_val+=g_RegulInfo_Ipart[index] 
         
         #KP is active only if Temperature is much higher
         temp_diff_KP = temp_diff - TEMP_KP_OFFSET
@@ -107,32 +122,33 @@ def Regul_Loop(index=0):
             
     #temparature lower than target
     else:
-        Regul_Loop.Ipart[index]+=temp_diff*KI_DOWN
-        if Regul_Loop.Ipart[index] < -20.0:
-            Regul_Loop.Ipart[index] = -20.0
+        g_RegulInfo_Ipart[index]+=temp_diff*KI_DOWN
+        if g_RegulInfo_Ipart[index] < -50.0:
+            g_RegulInfo_Ipart[index] = -50.0
             
-        pwm_val+=Regul_Loop.Ipart[index] 
+        pwm_val+=g_RegulInfo_Ipart[index] 
        
     if 95.0 < pwm_val :
         pwm_val = 95.0
-        Regul_Loop.OkCnt[index]=0
+        g_RegulInfo_OkCnt[index]=0
     elif pwm_val < 4.0:
         pwm_val = 4.0
-        Regul_Loop.OkCnt[index]=0
+        g_RegulInfo_OkCnt[index]=0
     elif abs(temp_diff) > 1 :
-        Regul_Loop.OkCnt[index]=0
+        g_RegulInfo_OkCnt[index]=0
     else:
-        Regul_Loop.OkCnt[index]+=1
-        if 0==(Regul_Loop.OkCnt[index] % 128):
-            write_FanSpeed( math.ceil(pwm_val) , index )
+        g_RegulInfo_OkCnt[index]+=1
+        if 127==(g_RegulInfo_OkCnt[index] % 128):
+            g_RegulInfo_OkPwm[index] = math.ceil(pwm_val) 
+            write_AllFanSpeed(g_RegulInfo_OkPwm)
 
     msg  = "%d: °C curr:%3.1f   " % (index,temp_curr)
     msg += "°C diff:%+3.1f   " % (temp_diff)
     msg += "pwm:%5.2f   " % (pwm_val)
-    msg += "OkCnt:%3d" % (Regul_Loop.OkCnt[index])
+    msg += "OkCnt:%3d" % (g_RegulInfo_OkCnt[index])
     print(msg)
     
-    set_FanSpeedValue(int(pwm_val) , index)
+    set_FanSpeed(int(pwm_val) , index)
     
     with open("out"+str(index)+".csv","a") as f:
         f.write(str(int(time.time()))+";"+str(temp_curr)+";"+str(pwm_val)+"\n")
@@ -140,43 +156,58 @@ def Regul_Loop(index=0):
     return
 
 #--------------------------
+
+def init():
+
+    global g_RegulInfo_Ipart
+    global g_RegulInfo_Fixpart
+    global g_RegulInfo_OkCnt
+    global g_RegulInfo_OkPwm
+    global g_NbGpu
+
+    g_RegulInfo_OkPwm = g_RegulInfo_Fixpart = read_AllFanSpeed()
+    g_NbGpu=g_RegulInfo_Fixpart.__len__()
+    
+    if 0 == g_NbGpu:
+        return -1
+       
+    for i in range(g_NbGpu):
+        i=i #ignore warning unused
+        g_RegulInfo_OkCnt.append(0)
+        g_RegulInfo_Ipart.append(0.0)
+        
+    return 0
+    
+def loop():
+    
+    global g_RegulInfo_GpuTemp
+    global g_RegulInfo_GpuFreq
+    
+    g_RegulInfo_GpuTemp=get_AllGpuTemp()
+    g_RegulInfo_GpuFreq=get_AllGpuFreq()
+
+    for i in range(g_NbGpu):
+        Regul_Loop(i)        
+    return 
+        
 def main():
-    #print( get_GpuTemp(0) )
-    #print( get_GpuTemp(1) )
-    #print( get_GpuTemp(2) )
-    #print( get_GpuTemp(3) )
-    
-    #print( get_GpuFreq(0) )
-    #print( get_GpuFreq(1) )
-    #print( get_GpuFreq(2) )
-    #print( get_GpuFreq(3) )
-    
-    #set_FanSpeedValue(38.3 , 1)
-    
-    set_FanSpeedManu( 0 )
-    set_FanSpeedManu( 1 )
-    set_FanSpeedManu( 2 )
-    set_FanSpeedManu( 3 )
-    
-    print( math.ceil(1.55) )
-    
+
+    if( init() != 0 ):
+        return -1
+        
     while 1:
         loop_time=15
         wait_time=loop_time - (time.time() % loop_time)
         print("wait for %5.2f sec" % (wait_time) )
         time.sleep(wait_time)
-        Regul_Loop(0)
-        Regul_Loop(1)
-        Regul_Loop(2)
-        Regul_Loop(3)
+        loop()
+        
+    for i in range(g_NbGpu):
+        set_FanSpeedAuto(i)
     return
 
 
 if __name__ == "__main__":
-    Regul_Loop.Ipart=[ 0.0 , 0.0 , 0.0 , 0.0 ]
-    Regul_Loop.Fixpart=[ read_FanSpeed(0) , read_FanSpeed(1) ,
-                         read_FanSpeed(2) , read_FanSpeed(3) ]
-    Regul_Loop.OkCnt=[ 0 , 0 , 0 , 0 ]
     main()
 
 
